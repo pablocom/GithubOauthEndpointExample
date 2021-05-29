6cb40040049e971f834a5ef018985f1c128469fe
Pablo <p.c.ramirez5150@gmail.com>
2021-05-29 13:26:55 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/GithubOauthEndpointController.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GithubOauthEndpoint.Controllers
+{
+    [ApiController]
+    [Route("user/signin/callback")]
+    public class GithubOauthEndpointController : ControllerBase
+    {
+        private readonly ILogger<GithubOauthEndpointController> _logger;
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public GithubOauthEndpointController(ILogger<GithubOauthEndpointController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<HttpResponseMessage>> Get([FromQuery(Name = "code")] string code, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(code))
+                return BadRequest("Code not present in request");
+            
+            var httpClient = httpClientFactory.CreateClient("GitHub");
+            var accessTokenRequestContent = new
+            {
+                client_id = "e2475c9bf2dc410d31a6", 
+                client_secret = "e8f235e039eccaa8dd43665b988b8680d567c7de", 
+                code
+            };
+
+            var result = await httpClient
+                .PostAsync("/login/oauth/access_token", JsonContent.Create(accessTokenRequestContent), cancellationToken)
+                .ConfigureAwait(false);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace GithubOauthEndpoint
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GithubOauthEndpoint", Version = "v1" });
+            });
+            services.AddHttpClient("GitHub", client =>
+            {
+                client.BaseAddress = new Uri("https://github.com");
+                client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+                client.DefaultRequestHeaders.Add("User-Agent", "HttpClientGithubFactory");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GithubOauthEndpoint v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
